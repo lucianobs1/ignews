32557015dfe7d5125d65c2823f2c24f48727b595
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-21 11:06:08 -0300

    trade SSR for SSG in HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -28,7 +28,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount} month</span>
           </p>
           
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"  />
@@ -37,10 +37,12 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1KqzIdHl99vviCKa5EXAN64n', {
-    expand: ['product']
-  });
+export const getStaticProps: GetStaticProps = async () => {
+  // const price = await stripe.prices.retrieve('price_1KqzIdHl99vviCKa5EXAN64n', {
+  //   expand: ['product']
+  // });
+
+  const price = await stripe.prices.retrieve('price_1KqzIdHl99vviCKa5EXAN64n');
 
   const product = {
     priceId: price.id,
@@ -53,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
