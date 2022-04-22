fcf0cbd1716c6463876f2f71f2fbad088a33509b
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-22 20:59:48 -0300

    added authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       className={styles.signInButton} 
       type="button"
+      onClick={() => signOut()}
     >
       <FaGithub color="#04D361"/>
-      Luciano Brandão
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
   </button>
 
@@ -20,6 +22,7 @@ export function SignInButton() {
     <button 
       className={styles.signInButton} 
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417"/>
       Sign in with Github
